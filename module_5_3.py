cfa73149db6556aef1c50b9beff4e65d8323b73b
Anton714 <an714.am@gmail.com>
2024-08-26 11:13:39 +0500

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,122 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        if isinstance (number_of_floors, int):
+            self.number_of_floors = number_of_floors
+
+        else:
+            print(f'Некорректно введен этаж: {number_of_floors}')
+            quit()
+
+        self.__len__()
+        self.__str__()
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return (f'Название: {self.name}, кол-во этажей: {self.number_of_floors}')
+
+    def __eq__(self, other):
+        if isinstance (other.number_of_floors, int):
+            return self.number_of_floors == other.number_of_floors
+
+        else:
+            print(f'Некорректно введен этаж: {other.number_of_floors}')
+            quit()
+
+
+    def __lt__(self, other):
+        if isinstance(other.number_of_floors, int):
+            return self.number_of_floors < other.number_of_floors
+
+        else:
+            print(f'Некорректно введен этаж: {other.number_of_floors}')
+            quit()
+
+
+    def __gt__(self, other):
+        if isinstance(other.number_of_floors, int):
+            return self.number_of_floors < other.number_of_floors
+
+        else:
+            print(f'Некорректно введен этаж: {other.number_of_floors}')
+            quit()
+
+
+
+    def __le__(self, other):
+        if isinstance(other.number_of_floors, int):
+            return self.number_of_floors <= other.number_of_floors
+
+        else:
+            print(f'Некорректно введен этаж: {other.number_of_floors}')
+            quit()
+
+
+    def __ge__(self, other):
+        if isinstance(other.number_of_floors, int):
+            return self.number_of_floors >= other.number_of_floors
+
+        else:
+            print(f'Некорректно введен этаж: {other.number_of_floors}')
+            quit()
+
+
+    def __ne__(self, other):
+        if isinstance(other.number_of_floors, int):
+            return self.number_of_floors != other.number_of_floors
+
+        else:
+            print(f'Некорректно введен этаж: {other.number_of_floors}')
+            quit()
+
+
+
+    def __add__(self, value):
+        if isinstance (value, int):
+            self.number_of_floors += value
+            return self
+
+        else:
+            print(f'Некорректно введен этаж: {value}')
+            quit()
+
+
+
+    def __iadd__(self, value):
+        self.__add__(value)
+        # self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        self.__add__(value)
+        #self.number_of_floors += value
+        return self
+
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
